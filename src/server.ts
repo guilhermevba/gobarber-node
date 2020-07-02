cdde6311f313ebcd83f7a9f60b728e97c83c08ed
Guilherme Azevedo <guilhermevba@gmail.com>
2020-07-01 21:50:08 -0300

    feat: route to fetch static files

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import routes from './routes'
+import path from 'path'
+import uploadConfig from './config/upload'
 import './database'
 
 const app = express();
 app.use(express.json())
+
+app.use('/images',express.static(uploadConfig.directory))
 app.get('/', (req, res) => {
   console.log('im alive')
   res.send('im alive')
